c44b8d2ac068d2812860779401d5116f46d01998
Ezequiel Magalhaes <zekes_243@hotmail.com>
2023-02-08 11:50:00 -0300

    Update in progress...

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import { Container, Heading } from '@chakra-ui/react';
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <h1>Olá mundo!</h1>
-    </div>
-  )
+	return (
+		<>
+			<Heading
+				fontSize={{base: '4xl', sm:'5xl', md: '6xl'}}
+				fontWeight='bold'
+				textAlign='center'
+				bgGradient='linear(to-l, #7928CA, #FF0080)'
+				bgClip='text'
+				mt={4}
+			>
+			Kanban Board<br/>To-do list
+			</Heading>
+			<Container maxWidth='container.lg' px={4} py={10}>
+			</Container>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ src/utils/enums.ts
@@ -0,0 +1,6 @@
+export enum ColumnType{
+    TO_DO='To do',
+    IN_PROGRESS='In progress',
+    BLOCKED='Blocked',
+    COMPLETED='Completed',
+}
